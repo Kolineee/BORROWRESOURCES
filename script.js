70cf33a72a8d3272a12522fa2e72d365b2700fda
Kolineee <mariecolleen049@gmail.com>
2024-04-16 08:28:48 -0700

    OKE KAAYO

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function toggleMenu() {
     var menuItems = document.querySelector('.navbar');
     menuItems.classList.toggle('active');
+}   
+let subMenu = document.getElementById("subMenu");
+
+function toggleMenu2(){
+  subMenu.classList.toggle("open-menu");
 }
 
+function toggleNotification() {
+    var notificationBox = document.getElementById("notificationBox");
+    notificationBox.style.display = notificationBox.style.display === "none" ? "block" : "none";
+  }
