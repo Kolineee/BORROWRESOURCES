78c2fbb028de23c8c810bb655e17b996f2e72881
Kolineee <mariecolleen049@gmail.com>
2024-04-17 09:49:45 -0700

    HHHHH

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ function toggleNotification() {
     var notificationBox = document.getElementById("notificationBox");
     notificationBox.style.display = notificationBox.style.display === "none" ? "block" : "none";
   }
+  function toggleDropdown(button) {
+    button.classList.toggle('clicked');
+    var dropdownContent = button.nextElementSibling;
+    dropdownContent.style.display === 'block' ? dropdownContent.style.display = 'none' : dropdownContent.style.display = 'block';
+  }
+  
