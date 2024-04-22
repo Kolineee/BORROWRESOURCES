d45dc7f2e502651039beecc10116378306530817
Kolineee <mariecolleen049@gmail.com>
2024-04-22 06:05:23 -0700

    ALMOSTTTTTTTTTT  DONE NAHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- script.js
+++ script.js
@@ -14,90 +14,116 @@ function toggleNotification() {
 }
 
 //Para functinable ang mga notif
-var hasReserved = false;
-var notificationTimeout;
+ var hasReserved = false;
+  var notificationTimeout;
 
-function reserve(buttonId, timeSlot) {
-  if (hasReserved) {
-    showAlert('You have already reserved a time slot for today.');
-    return;
-  }
+  // Function to reserve a time slot
+  function reserve(buttonId, timeSlot) {
+    if (hasReserved) {
+      showAlert('You have already reserved a time slot for today.');
+      return;
+    }
 
-  // Check if the time slot has at least 2 hours available
-  var startTime = timeSlot.split(' - ')[0];
-  var endTime = timeSlot.split(' - ')[1];
-  var startHour = parseInt(startTime.split(':')[0]);
-  var endHour = parseInt(endTime.split(':')[0]);
-  if (endHour - startHour < 2) {
-    showAlert('The projector must be reserved for at least 2 hours.');
-    return;
-  }
+    // Check if the time slot has at least 2 hours available
+    var startTime = timeSlot.split(' - ')[0];
+    var endTime = timeSlot.split(' - ')[1];
+    var startHour = parseInt(startTime.split(':')[0]);
+    var endHour = parseInt(endTime.split(':')[0]);
+    if (endHour - startHour < 2) {
+      showAlert('The projector must be reserved for at least 2 hours.');
+      return;
+    }
 
-  // Update button style to indicate reservation and disable it
-  var button = document.getElementById(buttonId);
-  button.textContent = 'Reserved';
-  button.classList.add('reserved');
-  button.disabled = true;
-
-  // Update reservation status
-  hasReserved = true;
-
-  // Display success message in notification
-  showNotification('Reservation Successful', 'Reserved time: ' + timeSlot);
-}
-
-
-// ERROR
-function showAlert(message) {
-  var notification = document.getElementById('notification');
-  var notificationTitle = notification.querySelector('h3');
-  var notificationMessage = notification.querySelector('p');
-
-  notificationTitle.textContent = 'Error';
-  notificationMessage.textContent = message;
-  notification.style.display = 'block';
-
-  // Disable all reserve buttons
-  var reserveButtons = document.querySelectorAll('.reserve-button');
-  reserveButtons.forEach(function(button) {
+    // Update button style to indicate reservation and disable it
+    var button = document.getElementById(buttonId);
+    button.textContent = 'Reserved';
+    button.classList.add('reserved');
     button.disabled = true;
-  });
 
-  // Clear previous timeout if exists
-  if (notificationTimeout) {
-    clearTimeout(notificationTimeout);
+    // Show the cancel button
+    var cancelButton = document.getElementById('cancel' + buttonId.slice(3));
+    cancelButton.style.display = 'inline-block';
+
+    // Update reservation status
+    hasReserved = true;
+
+    // Display success message in notification
+    showNotification('Reservation Successful', 'Reserved time: ' + timeSlot);
   }
 
-  // Hide the notification after 5 seconds
-  notificationTimeout = setTimeout(function() {
-    notification.style.display = 'none';
-    // Enable all reserve buttons after hiding the notification
+  // Function to cancel a reservation
+  function cancelReservation(buttonId, timeSlot) {
+    var button = document.getElementById(buttonId);
+    var cancelButton = document.getElementById('cancel' + buttonId.slice(3)); // Get the cancel button
+    cancelButton.style.display = 'none'; // Hide the cancel button
+    button.textContent = 'Reserve'; // Change the button text back to Reserve
+    button.classList.remove('reserved'); // Remove the reserved class
+    button.disabled = false; // Enable the button
+    hasReserved = false; // Update reservation status
+    showNotification('Reservation Cancelled', 'Cancelled time: ' + timeSlot); // Show cancellation notification
+    setTimeout(function() {
+      var notification = document.getElementById('notification');
+      notification.style.display = 'none';
+    }, 2000); // 2 seconds
+  }
+
+  // Function to show alert notification
+  function showAlert(message) {
+    var notification = document.getElementById('notification');
+    var notificationTitle = notification.querySelector('h3');
+    var notificationMessage = notification.querySelector('p');
+
+    notificationTitle.textContent = 'Error';
+    notificationMessage.textContent = message;
+    notification.style.display = 'block';
+
+    // Disable all reserve buttons
+    var reserveButtons = document.querySelectorAll('.reserve-button');
     reserveButtons.forEach(function(button) {
-      button.disabled = false;
+      button.disabled = true;
     });
-  }, 5000); // 5000 milliseconds = 5 seconds
-}
-//Succesful
-function showNotification(title, message) {
-  var notification = document.getElementById('notification');
-  var notificationTitle = notification.querySelector('h3');
-  var notificationMessage = notification.querySelector('p');
 
-  notificationTitle.textContent = title;
-  notificationMessage.textContent = message;
-  notification.style.display = 'block';
+    // Clear previous timeout if exists
+    if (notificationTimeout) {
+      clearTimeout(notificationTimeout);
+    }
 
-  // Clear previous timeout if exists
-  if (notificationTimeout) {
-    clearTimeout(notificationTimeout);
+    // Hide the notification after 5 seconds
+    notificationTimeout = setTimeout(function() {
+      notification.style.display = 'none';
+      // Enable all reserve buttons after hiding the notification
+      reserveButtons.forEach(function(button) {
+        button.disabled = false;
+      });
+    }, 5000); // 5000 milliseconds = 5 seconds
   }
 
-  // Hide the notification after 5 seconds
-  notificationTimeout = setTimeout(function() {
-    notification.style.display = 'none';
-    hasReserved = false; // Reset reservation status
-  }, 5000); // 5000 milliseconds = 5 seconds
-}
+  // Function to show success notification
+  function showNotification(title, message) {
+    var notification = document.getElementById('notification');
+    var notificationTitle = notification.querySelector('h3');
+    var notificationMessage = notification.querySelector('p');
+
+    notificationTitle.textContent = title;
+    notificationMessage.textContent = message;
+    notification.style.display = 'block';
+  }
+
+  // Function to cancel all reservations
+  function cancelAllReservations() {
+    var reserveButtons = document.querySelectorAll('.reserve-button');
+    var cancelButtons = document.querySelectorAll('.cancel-button');
+
+    reserveButtons.forEach(function(button, index) {
+      button.textContent = 'Reserve';
+      button.classList.remove('reserved');
+      button.disabled = false;
+      cancelButtons[index].style.display = 'none';
+    });
+
+    hasReserved = false;
+    showNotification('All Reservations Cancelled', 'All reservations have been cancelled.');
+  }
 
 // reserve button click
 function borrow(timeSlot) {
